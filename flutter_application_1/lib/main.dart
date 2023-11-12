1a9234f424ea6148d531c6d2bb5c10c4a33be04e
Dizaster735 <slavin0502@gmail.com>
2023-11-12 23:37:17 +0300

    Commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,96 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<int> numbers = [1, 2, 3, 4, 5];
+  final List<String> words = ['One', 'Two', 'Three', 'Four', 'Five'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Slavin Sergey'),
+        ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(  
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    width: 120,
-                    height: 200,
-                    color: Colors.black,                                             
-                  ),
-                  Container(               
-                    width: 120,
-                    height: 200,
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    color: Colors.green,                                  
-                  ),
-                ],
+            children: [
+              SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: numbers.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 5,
+                      width: 50,
+                      margin: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.all(10),
+                      color: Colors.blue,
+                      child: Text(
+                        numbers[index].toString(),
+                      ),
+                    );
+                  },
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(  
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    width: 120,
-                    height: 60,
-                    color: Colors.amber,                                             
-                  ),
-                  Container(               
-                    width: 120,
-                    height: 60,
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    color: Colors.brown,                                  
-                  ),
-                ],
-              ), 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(  
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    width: 120,
-                    height: 60,
-                    color: Colors.lightBlue,                                             
-                  ),
-                  Container(               
-                    width: 120,
-                    height: 60,
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    color: Colors.red,                                  
-                  ),
-                ],
-              ),              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(                              
-                    width: 120,
-                    height: 200,
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    color: Colors.grey,                              
-                  ),
-                  Container(               
-                    width: 120,
-                    height: 200,
-                    padding: const EdgeInsets.all(4),
-                    margin: const EdgeInsets.all(4),
-                    color: Colors.blueGrey,                                
-                  ),
-                ],
-              ),                            
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: words.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(words[index]),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
